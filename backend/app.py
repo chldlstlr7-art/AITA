595719e3b2fe2a3b14eabb4d62384a6708508e49
chldlstlr7-art <chldlstlr7@gmail.com>
2025-11-08 00:32:53 +0900

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,8 @@
+# backend/app.py (일부)
+
+# ... (다른 import)
+# (이전에 정의한 2_rag_service는 Colab 코드 로직상 사용되지 않음)
+# from services.2_rag_service import find_similar_documents 
+from services.3_analysis_service import perform_analysis_and_find_similar
+# ... (다른 서비스 import)
 
